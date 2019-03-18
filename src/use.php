c3ad4e078e768e3d820f6c9d6be5bfa5d97316eb
danb <barkovbox@mail.ru>
2019-03-18 23:09:54 +0700

    add file 'use'

--- src/use.php
+++ src/use.php
@@ -3,7 +3,7 @@ namespace VKRequestClass;
 
 require_once 'VKRequest.php';
 
-define('USER_TOKEN', '62331ec8745ced335f1d3c2a410f6a8975f8ee393cc3a0b923505fdfaa4bfe5446cf61713c491ea19c5f4');
+define('USER_TOKEN', 'vk_token_string');
 
 $request_test = new VKRequest(
     '62331ec8745ced335f1d3c2a410f6a8975f8ee393cc3a0b923505fdfaa4bfe5446cf61713c491ea19c5f4',
